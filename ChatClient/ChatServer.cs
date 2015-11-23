37fd40e89efa86dbf4b04c476c4ca310a45b37f0
Jonas Wallander <jwallander@gmail.com>
2015-11-23 09:53:24 +0100

    Owin debug middelware

--- ChatClient/ChatServer.cs
+++ ChatClient/ChatServer.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Diagnostics;
+using System.Web.Http;
 using Owin;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
 
 namespace JonasWallander.ChatClient
 {
@@ -9,9 +12,15 @@ namespace JonasWallander.ChatClient
         // ReSharper disable once UnusedMember.Global
         public void Configuration(IAppBuilder appBuilder)
         {
+            appBuilder.Use(new Func<AppFunc, AppFunc>(next => (async env =>
+            {
+                Debug.WriteLine("Begin Request");
+                await next.Invoke(env);
+                Debug.WriteLine("End Request");
+            })));
+
             var config = new HttpConfiguration();
             config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
-
             appBuilder.UseWebApi(config);
         }
     }
